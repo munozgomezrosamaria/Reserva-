91ae9e1dea42ee5ea678e7cc82723908f5a11970
Damarys-ToT <revelodamarys@gmail.com>
2026-02-22 09:56:06 -0500

    Mensaje de prueba para los commits: se agrego un mensaje en apps que no afecta el codigo, es meramente de prueba : )

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'apps.users'
+
+#Este mensaje es para probar el commit en git, no tiene ningun proposito en el codigo. ToT
