649f49acbfcfbbf33db30066697cd6619c951e98
munozgomezrosamaria <munozgomezrosamaria@gmail.com>
2026-03-24 14:33:25 -0500

    Fix ServiceSerializer image field type

--- Backend/apps/services/serializers.py
+++ Backend/apps/services/serializers.py
@@ -3,8 +3,6 @@ from .models import Service
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(required=False)
-
     class Meta:
         model = Service
         fields = ('id', 'title', 'description', 'image')
