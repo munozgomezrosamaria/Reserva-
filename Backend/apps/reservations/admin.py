7aa0699a903e25228cb6fb7eed1225c687a7b399
munozgomezrosamaria <munozgomezrosamaria@gmail.com>
2026-03-23 22:19:47 -0500

    Render

--- Backend/apps/reservations/admin.py
+++ Backend/apps/reservations/admin.py
@@ -1,16 +1,7 @@
-
-"""from django.contrib import admin
-from apps.reservations.models import Reservation
-# Register your models here.
-@admin.register(Reservation)
-class ReservationAdmin(admin.ModelAdmin):
-    list_display = ("description", "state")
-# Register your models here.
-"""
 from django.contrib import admin
 from .models import Reservation
 
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('date', 'number_persons', 'state', 'user', 'create_at')
+    list_display = ('date', 'number_persons', 'state', 'user', 'created_at')
 
 admin.site.register(Reservation, ReservationAdmin)
