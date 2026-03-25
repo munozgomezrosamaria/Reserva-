08c734bbef7e4fb13ce4808079244992dc95333c
munozgomezrosamaria <munozgomezrosamaria@gmail.com>
2026-03-25 14:40:10 -0500

    Increase timeout and make email backend configurable

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
