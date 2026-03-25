528b0202e03abc4794c0287df391268c3e5486b7
Damarys-ToT <revelodamarys@gmail.com>
2026-03-24 21:35:32 -0500

    fix: resolver conflictos users api

--- Backend/apps/users/api_urls.py
+++ Backend/apps/users/api_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .api_views import RegisterAPIView, UserProfileAPIView, AdminUserListAPIView
+from .api_views import RegisterAPIView, UserProfileAPIView, AdminUserListAPIView, PasswordResetRequestAPIView, PasswordResetConfirmAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='api_register'),
     path('profile/', UserProfileAPIView.as_view(), name='api_profile'),
     path('admin/', AdminUserListAPIView.as_view(), name='api_admin_users'),
+    path('password-reset/', PasswordResetRequestAPIView.as_view(), name='api_password_reset'),
+    path('password-reset/confirm/', PasswordResetConfirmAPIView.as_view(), name='api_password_reset_confirm'),
 ]
