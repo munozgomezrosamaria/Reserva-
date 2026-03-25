e8b65a39355c99e6e80b5889afd5c95b9d98f71d
munozgomezrosamaria <munozgomezrosamaria@gmail.com>
2026-03-24 21:16:40 -0500

    Implement decoupled password recovery flow

--- Backend/apps/users/api_urls.py
+++ Backend/apps/users/api_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .api_views import RegisterAPIView, UserProfileAPIView
+from .api_views import RegisterAPIView, UserProfileAPIView, PasswordResetRequestAPIView, PasswordResetConfirmAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='api_register'),
     path('profile/', UserProfileAPIView.as_view(), name='api_profile'),
+    path('password-reset/', PasswordResetRequestAPIView.as_view(), name='api_password_reset'),
+    path('password-reset/confirm/', PasswordResetConfirmAPIView.as_view(), name='api_password_reset_confirm'),
 ]
