3091b6dce9325abbe854df681047edb7a9d2ecd6
munozgomezrosamaria <munozgomezrosamaria@gmail.com>
2026-03-25 15:42:29 -0500

    Restore SMTP with connection timeout

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -167,10 +167,11 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10  # Seconds
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="tucorreo@gmail.com")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="abcd efgh ijkl mnop")
